242a0be4b67a3c7c9fba67cbd7711ca34b370ef7
Sayantan das <123sayantandas@gmail.com>
2025-03-29 20:12:55 +0530

    Product Order Page Done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,6 +14,7 @@ import {
 import Marketplace from "./Pages/Marketplace"
 import ProductListForm from "./Pages/ProductListForm"
 import { ErrorBoundary } from "react-error-boundary";
+import ProductOrderPage from "./Pages/ProductOrderPage";
 
 // Create a Layout component that includes Navbar and Footer
 const Layout = () => (
@@ -47,6 +48,7 @@ const router = createBrowserRouter([
     {path: "/", element: <Home />},
     {path: "marketplace", element: <Marketplace />},
     {path: "product-form", element: <ProductListForm />},
+    {path: "product-order", element: <ProductOrderPage />},
   ],
 }
 ])
