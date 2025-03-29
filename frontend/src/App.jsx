982d4f0ec1666c228a8af54efd7e8d0f820421ea
Sayantan das <123sayantandas@gmail.com>
2025-03-29 15:43:44 +0530

    Auction Page done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,39 @@
 import { Button } from "@/src/components/ui/button"
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Navigate,
+  Outlet,
+} from "react-router-dom";
+import Auctions from "./pages/Auctions.jsx"
+
+
+// Create a Layout component that includes Navbar and Footer
+const Layout = () => (
+  <>
+    {/* <Navbar /> */}
+    <Outlet />
+    {/* <Footer /> */}
+  </>
+);
+
+const router = createBrowserRouter([
+{
+  path: "/",
+  element: <Layout />,
+  children: [
+    // {path: "", element: <Home />},
+    {path: "auctions", element: <Auctions />},
+  ],
+}
+])
 
 function App() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-svh">
-      <Button>Click me</Button>
-    </div>
-  )
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App
