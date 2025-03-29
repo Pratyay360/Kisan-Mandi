3a6a3d8adf66ac197ea92dace518463a367de19c
debjyoti sarkar <debjyotisarkar929@gmail.com>
2025-03-29 19:11:47 +0530

    add ordermodel

---
+++ backend/src/models/orderModel.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const orderSchema = new mongoose.Schema({
+
+    transactionId: {
+        type: String,
+        required: true,
+    },
+
+    amount: {
+        type: Number,
+        required: true,
+    },
+     productId: {
+        type: String,
+        required: true,
+    },
+
+    userId: {
+        type: String,
+        required: true,
+    },
+
+    merchantId: {
+        type: String,
+        required: true,
+    },
+
+    status: {
+        type: String,
+        enum: ["pending", "success", "failed"],
+        default: "pending",
+    },
+});
+
+export default mongoose.model("Order", orderSchema);
