28bfd201e2b4632c6ca2f0aaaae6f9e19c18b327
debjyoti sarkar <debjyotisarkar929@gmail.com>
2025-03-30 09:16:32 +0530

    edit user route and farmer product page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -24,6 +24,7 @@ import Message from "./Pages/Message";
 import ProductPage from "./Pages/ProductPage";
 import AboutUs from "./Pages/AboutUs";
 import FarmerProfile from "./Pages/FarmerProfile";
+import FarmerProductPage from "./Pages/farmerProductPage";
 // Create a toast context for application-wide error notifications
 export const ToastContext = createContext({
   showError: (message) => {},
@@ -99,6 +100,7 @@ const router = createBrowserRouter([
       { path: "community-forum", element: <CommunityForum /> },
       { path: "ask-question-form", element: <AskQuestionForm /> },
       { path: "profile/:id", element: <FarmerProfile /> },
+      { path: "farmer-product-page/:id", element: <FarmerProductPage /> },
     ],
   },
 ]);
